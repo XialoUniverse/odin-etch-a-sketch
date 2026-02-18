0dd4cebf54f3ccd4f32ccd85a4d0166042d3b50f
Xialo Universe <173575491+XialoUniverse@users.noreply.github.com>
2026-02-18 04:25:06 +0300

    Make the borders of the pixels change based on size

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ let container = document.querySelector('.container');
 
 function resetCanvas(size) {
     container.innerHTML = '';
+    let squareClass;
+    if (size > 50) {
+        container.classList.add('container-border');
+        squareClass = 'black-border-square';
+    } else {
+        container.classList.remove('container-border');
+        squareClass = 'square';
+    }
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             let square = document.createElement('div');
-            square.classList.add('square');
+            square.classList.add(squareClass);
             square.style.height = `${480 / size}px`;
             square.style.width = `${480 / size}px`;
             square.addEventListener('mouseover', (event) => {
-                square.classList.remove('square');
+                square.classList.remove(squareClass);
                 square.classList.add('activated-square');
             })
             container.appendChild(square);
