2ed61839bd1285e835dc083da26d68d7602f5fe4
Xialo Universe <173575491+XialoUniverse@users.noreply.github.com>
2026-02-18 04:16:16 +0300

    Make the size of the canvas variable

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function resetCanvas(size) {
         for (let j = 0; j < size; j++) {
             let square = document.createElement('div');
             square.classList.add('square');
+            square.style.height = `${480 / size}px`;
+            square.style.width = `${480 / size}px`;
             square.addEventListener('mouseover', (event) => {
                 square.classList.remove('square');
                 square.classList.add('activated-square');
