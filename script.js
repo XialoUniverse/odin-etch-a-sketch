d708e8e4fbfef18e93359adc70cd2c6b541309dd
Xialo Universe <173575491+XialoUniverse@users.noreply.github.com>
2026-02-18 03:24:49 +0300

    Add hover effect to make pixels look drawn on

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         let square = document.createElement('div');
         square.classList.add('square');
+        square.addEventListener('mouseover', (event) => {
+            square.classList.remove('square');
+            square.classList.add('activated-square');
+        })
         container.appendChild(square);
     }
 }
